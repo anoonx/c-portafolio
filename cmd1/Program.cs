2e147b83640a3169fba4592773ea931ef780a499
Mario sanchez <mariomsj@gmail.com>
2026-05-23 17:39:34 -0500

    primer commit 2026

---
+++ cmd1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmd1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, soy Mario y sé programar");
+            Console.ReadLine();
+        }
+    }
+}
